bf5ed644a250a19aac84c4af80481c96ae743670
Olegych76 <f-o-r-j-o-b@yandex.ru>
2024-08-14 17:55:21 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Олег'
+print(name)
+
+age = 64
+print("Возраст", age)
+
+new_value = 3  # любое значение
+age = age + new_value
+print("Изменённый возраст", age)
+
+is_student = True
+print(is_student)
